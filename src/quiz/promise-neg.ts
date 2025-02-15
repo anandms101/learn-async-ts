67a2712e6a8195915e316653de27ba4cdd92ff1a
ANAND MOHAN SINGH <anandmohan.singh101@gmail.com>
2025-02-15 13:54:06 -0500

    added promise all in sum2darray and primseneg

--- src/quiz/promise-neg.ts
+++ src/quiz/promise-neg.ts
@@ -1,5 +1,19 @@
 const array2D_3 = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, -9]
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, -9],
 ];
+
+function hasNegative(row: number[]): Promise<boolean> {
+  return new Promise((resolve) => {
+    resolve(row.some((num) => num < 0));
+  });
+}
+
+Promise.all(array2D_3.map((row) => hasNegative(row)))
+  .then((results) => {
+    results.forEach((found, index) => {
+      if (found) console.log("Row with negative:", array2D_3[index]);
+    });
+  })
+  .catch((err) => console.error(err));
--- src/quiz/promise-sum-2darray.ts
+++ src/quiz/promise-sum-2darray.ts
@@ -1,5 +1,21 @@
 const array2D_1 = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
 ];
+
+function computeRowSum(row: number[]): Promise<number> {
+  return new Promise((resolve) => {
+    resolve(row.reduce((acc, num) => acc + num, 0));
+  });
+}
+
+function computeSum2DArrayConcurrent(arr: number[][]): Promise<number> {
+  return Promise.all(arr.map((row) => computeRowSum(row))).then((rowSums) =>
+    rowSums.reduce((acc, cur) => acc + cur, 0)
+  );
+}
+
+computeSum2DArrayConcurrent(array2D_1)
+  .then((total) => console.log("Total sum (concurrent):", total))
+  .catch((err) => console.error(err));
